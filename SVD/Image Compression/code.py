240ee1e8b153dcee7aed89ea5a3bcc6512b38758
Ashis Kumar Chanda <ashis415@yahoo.com>
2017-06-07 00:44:12 -0400

    Update code.py

--- SVD/Image Compression/code.py
+++ SVD/Image Compression/code.py
@@ -3,30 +3,42 @@ import scipy.linalg as SL
 import matplotlib.pyplot as plt
 
 from scipy.misc import imread
-im = imread("lenabw.jpg") # Load an color image 
-Pxx = im[:,:, 0]          # taking only one dimensional values
+im = imread("lenabw.jpg")       # Load an color image 
+Pxx = im[:,:, 0]                # taking only one dimensional values
 
 plt.imshow(Pxx)
-#plt.plot(Pxx)  # it will show the values in graph plotting
-plt.show()      # showing orginal image
+#plt.plot(Pxx)                  # it will show the values in graph plotting
+plt.show()                      # showing orginal image
 
+print (Pxx.shape)  # original matrix shape: (554L, 507L)
 U, s, Vh = SL.svd(Pxx, full_matrices=False)
 assert np.allclose(Pxx, np.dot(U, np.dot( np.diag(s), Vh)  ) )
 #Returns True if two arrays are element-wise equal within a tolerance
 
-print ("s ", s.shape ) # 507 by 1
-s1 = [k for k in s]    # storing all s values for further checking
-s[8:] = 0  # taking only first few elements and make all others zero 
+#old matrix size
+print ("U ", U.shape )               #('U ', (554L, 507L))
+print ("s ", s.shape )               #('s ', (507L,))
+print ("Vh ", Vh.shape )             #('Vh ', (507L, 507L))
+
+
+# print s and look, where the value change dramatically
+# set new_dimension on the index
+# print ( s[:30])
+
+new_dimension = 30
+s1 = [k for k in s]          # storing all s values for further checking
+s[new_dimension:] = 0        # taking only first few elements and make all others zero 
 
 new_a = np.dot(U, np.dot(np.diag(s), Vh)) # getting new image
 plt.imshow(new_a)
 plt.show()
 
 # now reducing matrix size
-U = U[:, :8]            # (554, 8)
-Vh = Vh [ :8, :]        # (8, 507)
-s = s[:8]               # (8,1)
+U = U[:, :new_dimension]              # (554, 8)
+s = s[:new_dimension]                 # (8,1)
+Vh = Vh [ :new_dimension, :]          # (8, 507)
+
 new_a = np.dot(U, np.dot(np.diag(s), Vh))
-print(new_a.shape )  # (554, 507) #getting almost similar type matrix
+print(new_a.shape )                   # (554, 507) #getting almost similar type matrix
 
 plt.imshow(new_a)
