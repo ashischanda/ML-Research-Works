c17c8755ad393c0fdaf0546e2c94e30d6a8ef467
Ashis Kumar Chanda <ashis415@yahoo.com>
2019-03-05 10:03:22 -0500

    Update code.py

--- SVD/Image Compression/code.py
+++ SVD/Image Compression/code.py
@@ -1,3 +1,6 @@
+# Matrix reduction using SVD
+# We have a picture of Big matrix. 
+# We want find a small matrix that will contain same picture quality
 import numpy as np
 import scipy.linalg as SL
 import matplotlib.pyplot as plt
@@ -23,7 +26,7 @@ print ("Vh ", Vh.shape )             #('Vh ', (507L, 507L))
 
 # print s and look, where the value change dramatically
 # set new_dimension on the index
-# print ( s[:30])
+# print ( s[: new_dimension])
 
 new_dimension = 30
 s1 = [k for k in s]          # storing all s values for further checking
@@ -34,11 +37,28 @@ plt.imshow(new_a)
 plt.show()
 
 # now reducing matrix size
-U = U[:, :new_dimension]              # (554, 8)
-s = s[:new_dimension]                 # (8,1)
-Vh = Vh [ :new_dimension, :]          # (8, 507)
+U = U[:, :new_dimension]              # (554,           new_dimension)
+s = s[:new_dimension]                 # (new_dimension, 1)
+Vh = Vh [ :new_dimension, :]          # (new_dimension, 507)
 
 new_a = np.dot(U, np.dot(np.diag(s), Vh))
 print(new_a.shape )                   # (554, 507) #getting almost similar type matrix
 
 plt.imshow(new_a)
+
+'''
+Look, at the beggining we had a matrix of (554L, 507L)
+We have to remember (554 X 507) = 280,878 cells
+
+Now, we have new U, s, Vh
+We need to remember 554 * 30 = 16620
+                    507 * 30 = 15210
+                             = 30
+          Total around       = 30,000 cells
+          
+          
+But, if we need the old matrix, we can multiply new U, s, Vh and get new_a
+------------------------
+In this way, we can save space and keep same source of information
+'''
+
